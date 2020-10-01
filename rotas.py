e870f6b1c414669a9ecb12e2b7d8e4f44da79211
NicolasM3 <niicck992@gmail.com>
2020-10-01 12:15:17 -0300

    adicionando logs

--- rotas.py
+++ rotas.py
@@ -1,6 +1,15 @@
 from flask import Flask, jsonify
+import logging
 
 from scraping import scraping
+
+
+LOG_FORMAT = "%(levelname)s : %(filename)s : %(asctime)s : %(message)s"
+logging.basicConfig(filename=".logs/ApiLogs",
+                    level = logging.INFO,
+                    format= LOG_FORMAT,
+                    filemode= "w")
+logger = logging.getLogger()
 
 scrap = scraping()
 
@@ -8,12 +17,26 @@ app = Flask("Libras Acedemy")
 
 @app.route("/getDict/<latter>", methods=["GET"])
 def getDictionary(latter):
+    logger.info(f"getDictionary of {latter}")
+
     words = scrap.getDictionary(latter)
+
+    if(words == None):
+        logger.info(f"Letra não encontrda")
+        return jsonify({"None" : "Nenhuma palavra encontrada com essa letra"})
+
     return jsonify(words)
 
 @app.route("/getGif/<word>", methods=["GET"])
 def getGif(word):
+    logger.info(f"getGif of {word}")
+
     link = {"link": scrap.getGif(word)}
+
+    if link == None:
+        logger.info(f"Palavra não encontrda")
+        return jsonify({"None" : "Nenhnuma palavra encotrada"})
+        
     return jsonify(link)
 
 app.run()
